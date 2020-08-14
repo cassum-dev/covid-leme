e5c3dde1b4575c310f8139443ff7b595931ba8a3
Pedro Pozzi Ferreira <pedropozzif@gmail.com>
2020-08-14 19:09:26 -0300

    Add PieChart Component

---
+++ app/src/components/PieChart.vue
@@ -0,0 +1,37 @@
+<script>
+    import { Pie } from "vue-chartjs"
+    import  { getPieCovidData }  from "../services.js"
+
+    export default {
+        extends: Pie,
+        data () {
+            return {
+                chartdata: {
+                    labels: [
+                        'Casos ativos',
+                        'Casos recuperados',
+                        'Óbitos'
+                    ],
+                    datasets: [
+                        {
+                            label: 'Covid Leme',
+                            backgroundColor: ['#ef4b3f', '#00a85a', '#606062'],
+                            data: [
+                                getPieCovidData('active_cases'),
+                                getPieCovidData('total_recovered'),
+                                getPieCovidData('total_deaths')
+                            ]
+                        }
+                    ],
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false
+                }
+            }
+        },
+        mounted () {
+            this.renderChart(this.chartdata, this.options)
+        }
+    }
+</script>
