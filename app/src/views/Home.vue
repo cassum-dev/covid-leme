485e9db16514fc200148ed127190a9f92a036af6
Pedro Pozzi Ferreira <pedropozzif@gmail.com>
2020-08-14 15:32:23 -0300

    Add Home View

---
+++ app/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <section>
+        <h1>Covid Leme</h1>
+        <h4>Última atualização em 14/08/2020</h4>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
