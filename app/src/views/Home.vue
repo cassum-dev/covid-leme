dd9b6ba9692d6e7af081f709a5a3691e475ba72e
Petrius Lima <petrius.lima@cassum.dev>
2020-08-14 17:20:20 -0300

    Change lastUpdatedDate to be dynamic

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
     <section>
         <h1>Covid Leme</h1>
-        <h4>Última atualização em 14/08/2020</h4>
+        <h4>Última atualização em {{ lastUpdatedDate }}</h4>
     </section>
 </template>
 
 <script>
+    import CovidData from '../datasources/covid.json';
+
     export default {
-        name: "Home"
+        name: "Home",
+        computed: {
+            lastUpdatedDate: function() {
+                let lastDay = CovidData.find(element => element.is_last == true);
+
+                return lastDay.date;
+            }
+        }
     }
 </script>
 
