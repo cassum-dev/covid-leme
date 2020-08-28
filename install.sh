b3178bf4c74d1198fc2a19735b1c9f6ee1274fb8
Petrius Lima <petrius.lima@cassum.dev>
2020-08-28 14:13:27 -0300

    Fix install.sh after directory change for heroku

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-docker run --rm --interactive --tty --volume $PWD/app/:/app --workdir /app node:14.5.0-alpine sh -c "npm install"
+docker run --rm --interactive --tty --volume $PWD/:/app --workdir /app node:14.5.0-alpine sh -c "npm install"
 
 docker-compose up --build
