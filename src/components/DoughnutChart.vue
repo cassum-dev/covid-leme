494e6a5c9a60487ac9e6edbb93fc38e9405e7a1e
Gustavo Henrique Pinto <gustavo.pinto@cassum.dev>
2021-02-23 15:57:31 -0300

    Add base Doughnut chart

---
+++ src/components/DoughnutChart.vue
@@ -0,0 +1,29 @@
+<script>
+    import { Doughnut } from "vue-chartjs"
+
+    export default {
+        extends: Doughnut,
+        props: ['labels', 'colors', 'data'],
+        data () {
+            return {
+                chartdata: {
+                    labels: this.labels,
+                    datasets: [
+                        {
+                            label: 'Covid Leme',
+                            backgroundColor: this.colors,
+                            data: this.data,
+                        }
+                    ],
+                },
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false
+                }
+            }
+        },
+        mounted () {
+            this.renderChart(this.chartdata, this.options)
+        }
+    }
+</script>
