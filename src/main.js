58670ea47d15048d9106919fb2da5aeaf01f4b63
Gustavo Henrique Pinto <gustavo.pinto@cassum.dev>
2021-02-24 15:08:40 -0300

    Add Chartjs Datalabels plugin to project

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import ChartJsPluginDataLabels from 'chartjs-plugin-datalabels';
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(ChartJsPluginDataLabels);
 
 Vue.config.productionTip = false
 
